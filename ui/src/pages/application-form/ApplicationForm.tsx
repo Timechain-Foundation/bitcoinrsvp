05746415b03f18f5c52df9c00fcc2fb0017b6df1
Randall Naar <rnd.naar@gmail.com>
2025-03-16 07:38:55 -0400

    Added submission confirmation screen.

--- ui/src/pages/application-form/ApplicationForm.tsx
+++ ui/src/pages/application-form/ApplicationForm.tsx
@@ -5,6 +5,7 @@ import TextArea from "../../components/text-area/TextArea";
 import TextInput from "../../components/text-input/TextInput";
 import { getGroup, MembershipApplication, postApplication } from "../../sdk";
 import styles from "./ApplicationForm.module.css";
+import classNames from "classnames";
 
 function ApplicationForm() {
   // TODO: Get this from path
@@ -13,7 +14,8 @@ function ApplicationForm() {
   let [group, setGroup] = useState<any>(undefined);
   let [form, setForm] = useState<any>({});
   let [loading, setLoading] = useState(false);
-  let [error, setError] = useState<string | undefined>(undefined)
+  let [error, setError] = useState<string | undefined>(undefined);
+  let [submitted, setSubmitted] = useState(false);
 
   async function postApplicationAsync(application: MembershipApplication) {
     try {
@@ -44,67 +46,99 @@ function ApplicationForm() {
     "Invitees can reserve only one ticket. Reservations are first come-first served, Ticket confirmations are final and cannot be transferred, as invitations are approved on a case-by-case basis.";
 
   return (
-    <div className={styles.content}>
-      <LogoSvg className={styles.logoSvg} />
-      <h1 className={styles.title}>{title}</h1>
-      <div className={styles.panel}>
-        <div>
-          <h2>Event Description</h2>
-          <p>{description}</p>
+    <div className={classNames(styles.content, styles.submittedContent)}>
+      {submitted ? (
+        <div className={classNames(styles.confirm)}>
+          <LogoSvg className={styles.logoSvg} />
+          <div className={styles.confirmationPanel}>
+            <div>
+              <h2>Registration Confirmation</h2>
+              <p>
+                You have registered to participate in the Bitcoin Dinner Club.
+                Thanks for your interest, but registration does not guarantee an
+                invitation. Ensure the accuracy of your name and E-mail address.
+                If you made a mistake, please re-register. All registrations
+                require approval. To ensure receipt of our response, add
+                prem@premlee.com to your E-mail contacts.
+              </p>
+            </div>
+          </div>
         </div>
-        <div>
-          <h2>Rules and Regulations</h2>
-          <p>{rules}</p>
-        </div>
-      </div>
-      <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
-        <TextInput
-          label="Name"
-          placeholder="Your full name"
-          onChange={(e: any) =>
-            setForm({
-              ...form,
-              name: e?.target?.value,
-            })
-          }
-        />
-        <TextInput
-          label="Email"
-          placeholder="Your email address"
-          onChange={(e: any) =>
-            setForm({
-              ...form,
-              email: e?.target?.value,
-            })
-          }
-        />
-        {group?.questions.map(
-          (q: { id: number; question: string }, index: number) => {
-            return (
-              <TextArea
-                key={index}
-                label={q.question}
-                placeholder={q.question}
-                onChange={(e: any) =>
-                  setForm({
-                    ...form,
-                    questions: [
-                      ...(form?.questions?.filter(
-                        (existingQuestion: { id: number; answer: string }) =>
-                          existingQuestion.id != q.id
-                      ) || []),
-                      { id: q.id, answer: e.target.value },
-                    ],
-                  })
-                }
-              />
-            );
-          }
-        )}
+      ) : (
+        <>
+          <LogoSvg className={styles.logoSvg} />
+          <h1 className={styles.title}>{title}</h1>
+          <div className={styles.panel}>
+            <div>
+              <h2>Event Description</h2>
+              <p>{description}</p>
+            </div>
+            <div>
+              <h2>Rules and Regulations</h2>
+              <p>{rules}</p>
+            </div>
+          </div>
+        </>
+      )}
+      {!submitted && (
+        <form className={styles.form} onSubmit={(e) => e.preventDefault()}>
+          <TextInput
+            label="Name"
+            placeholder="Your full name"
+            onChange={(e: any) =>
+              setForm({
+                ...form,
+                name: e?.target?.value,
+              })
+            }
+          />
+          <TextInput
+            label="Email"
+            placeholder="Your email address"
+            onChange={(e: any) =>
+              setForm({
+                ...form,
+                email: e?.target?.value,
+              })
+            }
+          />
+          {group?.questions.map(
+            (q: { id: number; question: string }, index: number) => {
+              return (
+                <TextArea
+                  key={index}
+                  label={q.question}
+                  placeholder={q.question}
+                  onChange={(e: any) =>
+                    setForm({
+                      ...form,
+                      questions: [
+                        ...(form?.questions?.filter(
+                          (existingQuestion: { id: number; answer: string }) =>
+                            existingQuestion.id != q.id
+                        ) || []),
+                        { id: q.id, answer: e.target.value },
+                      ],
+                    })
+                  }
+                />
+              );
+            }
+          )}
 
-        <p className={styles.error}>{error}</p>
-        <Button className={styles.submitButton} onClick={() => postApplicationAsync(form)} loading={loading}>Submit Registration</Button>
-      </form>
+          <p className={styles.error}>{error}</p>
+          <Button
+            className={styles.submitButton}
+            onClick={() => {
+              postApplicationAsync(form);
+              setSubmitted(true);
+            }}
+            loading={loading}
+          >
+            Submit Registration
+          </Button>
+        </form>
+      )}
     </div>
   );
 }
